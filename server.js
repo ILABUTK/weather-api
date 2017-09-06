852d22491db03e54c5376d72a3a2a4a3469f76d8
ilab <ilab.utk@gmail.com>
2017-09-05 23:30:25 -0400

    add path to pen

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const fs = require('fs'); // for https fs
 var options = {
   // key: fs.readFileSync('/etc/letsencrypt/live/jasmin.engr.utk.edu/privkey.pem'),
   // cert: fs.readFileSync('/etc/letsencrypt/live/jasmin.engr.utk.edu/fullchain.pem')
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
+  key: fs.readFileSync('/var/www/vhosts/jasmin/nodeserver/weather-api/key.pem'),
+  cert: fs.readFileSync('/var/www/vhosts/jasmin/nodeserver/weather-api/cert.pem')
 };
 
 const limiter = new RateLimit({
